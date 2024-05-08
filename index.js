5e8c938f50e4cf570bceb133f76fc1e8b1ab7e2a
aycughj <gs1saltos29@hotmail.com>
2024-05-08 07:08:40 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function canJump(nums) {
-  let lastPos = nums.length - 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    if (i + nums[i] >= lastPos) lastPos = i;
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
   }
-  return lastPos === 0;
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
 }
