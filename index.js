37beaa4f67d373accb9859f1b5e66f7c0d69fd19
aycughj <gs1saltos29@hotmail.com>
2024-05-13 16:08:38 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function findMaxLength(nums) {
+function lengthOfLongestSubstring(s) {
   const map = new Map();
-  map.set(0, -1);
-  let count = 0;
   let maxLength = 0;
-  for (let i = 0; i < nums.length; i++) {
-    count += nums[i] === 1 ? 1 : -1;
-    if (map.has(count)) {
-      maxLength = Math.max(maxLength, i - map.get(count));
-    } else {
-      map.set(count, i);
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) {
+      left = Math.max(left, map.get(s[right]) + 1);
     }
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
   }
   return maxLength;
 }
