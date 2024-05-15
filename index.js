93782cc36912c7580531679a2bab9587dede03db
aycughj <gs1saltos29@hotmail.com>
2024-05-16 03:30:19 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const randomNumber = Math.floor(Math.random() * 10) + 1;
+function findTheDifference(s, t) {
+  let result = 0;
+  for (const char of s) {
+    result ^= char.charCodeAt(0);
+  }
+  for (const char of t) {
+    result ^= char.charCodeAt(0);
+  }
+  return String.fromCharCode(result);
+}
